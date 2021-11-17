d8a3be38965c30480d331506c372122fbef8a85f
MARCELO FIDELES <fidelis.marcelo@gmail.com>
2021-11-17 16:19:13 -0300

    Commit 7

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
 import React from "react"
 import Layout from "../component/Layout"
+import { page, text } from "../examples/about.module.css"
 
 export default function about() {
   return (
     <Layout>
-      <div className="about-heading">
+      <div className={page}>
         <h1>About</h1>
         <h2>About Page</h2>
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        </p>
       </div>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../component/Layout"
+import { page, text } from "../examples/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <div className="home-heading">
-        <h1>Hello Gatsby!</h1>
-        <h1 style={{ color: "grey", textTransform: "capitalize" }}>
-          home page
-        </h1>
-        <h1>Hello WORLD!</h1>
+      <div className={page}>
+        <h1>home page</h1>
+        <h1>Hello world</h1>
         <a href="https://www.gatsbyjs.com/docs/how-to/">Gatsby Docs</a>
+        <p className={text}>
+          lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        </p>
       </div>
     </Layout>
   )
