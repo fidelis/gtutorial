e1e2f5c03a9a9b408858d89a3235ae9761b6637e
MARCELO FIDELES <fidelis.marcelo@gmail.com>
2021-11-17 15:39:44 -0300

    Commit 7

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,10 @@ import Layout from "../component/Layout"
 export default function about() {
   return (
     <Layout>
-      <h2>About Page</h2>
+      <div className="about-heading">
+        <h1>About</h1>
+        <h2>About Page</h2>
+      </div>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,14 @@ import Layout from "../component/Layout"
 export default function Home() {
   return (
     <Layout>
-      <h1 style={{ color: "green", textTransform: "capitalize" }}>home page</h1>
-      <h1>Hello WORLD!</h1>
-      <a href="https://www.gatsbyjs.com/docs/how-to/">Gatsby Docs</a>
+      <div className="home-heading">
+        <h1>Hello Gatsby!</h1>
+        <h1 style={{ color: "grey", textTransform: "capitalize" }}>
+          home page
+        </h1>
+        <h1>Hello WORLD!</h1>
+        <a href="https://www.gatsbyjs.com/docs/how-to/">Gatsby Docs</a>
+      </div>
     </Layout>
   )
 }
